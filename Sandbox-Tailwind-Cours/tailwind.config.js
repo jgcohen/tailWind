16f1e946d8964bb40f006c7411db1ded04eb9fae
jgcohen <75163282+jgcohen@users.noreply.github.com>
2021-04-09 16:23:04 +0200

    .

---
+++ Sandbox-Tailwind-Cours/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports= {
+    darkMode:'class',
+    theme:{
+        extend:{
+            spacing:{
+                enorme:'1500px'
+            },
+            colors:{
+                customColor:'#000'
+            }
+        }
+    }
+}
